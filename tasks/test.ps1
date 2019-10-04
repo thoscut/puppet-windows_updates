0cd3550a73d4e278481f6e18be6e4e570b815669
Administrator <Administrator@dreamworx.nl>
2019-10-04 11:32:52 +0200

    test task

---
+++ tasks/test.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory = $True)] [String] $kb,
+  [Parameter(Mandatory = $True)] [String] $_installdir
+)
+
+Write-Host $_installdir
+Get-ChildItem "$_installdir/windows_updates/files/"
+#Get-WUHistory
