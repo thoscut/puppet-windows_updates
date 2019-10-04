f5f3267fc022312193177d7c358ffa277592abb6
Administrator <Administrator@dreamworx.nl>
2019-10-04 11:47:43 +0200

    test import module

--- tasks/test.ps1
+++ tasks/test.ps1
@@ -4,6 +4,7 @@ Param(
   [Parameter(Mandatory = $True)] [String] $_installdir
 )
 
-Write-Host $_installdir
-Get-ChildItem "$_installdir/windows_updates/files/"
-#Get-WUHistory
+#Write-Host $_installdir
+#Get-ChildItem "$_installdir/windows_updates/files/"
+Import-Module -Name "$_installdir/windows_updates/files/PSWindowsUpdate"
+Get-WUHistory
